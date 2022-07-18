65ae77ceb25b7204bc87b753a64c43dd74ab2be6
Amanda Custódio <amandaccustodio@hotmail.com>
2022-07-18 10:14:35 -0300

    Criada tabuada em cs

---
+++ Exercicio-tabuada/Exercicio-tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fator, i, produto;
+
+            Console.WriteLine("Digite o número que deseja saber a tabuada: ");
+            fator = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Para " + fator + " x " + i);
+                produto = fator * i;
+                Console.WriteLine("O produto é: " + produto);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
